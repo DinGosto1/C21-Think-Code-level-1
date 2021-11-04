99bb17ae3d1da4c5a36e8b9ba95ac998bc5d6e3c
Din Gosto <din.gosto@student.rocva.nl>
2021-11-04 15:19:27 +0100

    eindopdracht afgemaakt

--- Opdracht5/opdracht_05.py
+++
@@ -1,6 +0,0 @@
-def favoriete_eten (name):
- print("een van mijn favorieten eten is",name)
-
-favoriete_eten("pasta")
-favoriete_eten("vis")
-favoriete_eten("soep")
---
+++ eindopdracht/opdracht_05.py
@@ -0,0 +1,6 @@
+def dingen_wat_ik_leuk_vind_om_te_doen (name):
+ print("een van de dingen die leuk vind om te doen is",name)
+
+dingen_wat_ik_leuk_vind_om_te_doen("voetballen")
+dingen_wat_ik_leuk_vind_om_te_doen("reizen")
+dingen_wat_ik_leuk_vind_om_te_doen("feesten")

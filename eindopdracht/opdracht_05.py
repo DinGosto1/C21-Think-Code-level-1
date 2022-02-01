8d6832a361c26e835bad994c1026e5409fc35bd8
Din Gosto <din.gosto@student.rocva.nl>
2022-02-01 12:18:12 +0100

    eindopdracht verbeterd

--- eindopdracht/opdracht_05.py
+++ eindopdracht/opdracht_05.py
@@ -1,6 +1,14 @@
+from tkinter.messagebox import QUESTION
+
+
 def dingen_wat_ik_leuk_vind_om_te_doen (name):
  print("een van de dingen die leuk vind om te doen is",name)
 
 dingen_wat_ik_leuk_vind_om_te_doen("voetballen")
 dingen_wat_ik_leuk_vind_om_te_doen("reizen")
 dingen_wat_ik_leuk_vind_om_te_doen("feesten")
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+Question = input("wat is mijn naam")
